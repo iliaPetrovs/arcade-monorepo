9852c36d1d2bfc850d8bc5432ccd40f3955fecde
Ilja Petrovs <ilja.petrovs@transferwise.com>
2023-06-29 21:16:12 +0100

    pre-purge

--- apps/backend/medusa-config.js
+++ apps/backend/medusa-config.js
@@ -34,9 +34,12 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL =
-    `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-    `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+// const DATABASE_URL =
+//     process.env.DEPLOYMENT === "production"
+//         ? `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
+//           `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+//         : process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -75,7 +78,11 @@ const modules = {
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-    database_extra: { ssl: { rejectUnauthorized: false } },
+    ...(process.env.DEPLOYMENT === "production" && {
+        database_extra: {
+            ssl: { rejectUnauthorized: false },
+        },
+    }),
     jwtSecret: process.env.JWT_SECRET,
     cookieSecret: process.env.COOKIE_SECRET,
     store_cors: STORE_CORS,
